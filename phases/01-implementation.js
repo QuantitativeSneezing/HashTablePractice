631e9282feec53d77cf2c993e29510a130186a16
Jason <jatsburgern@gmail.com>
2022-07-08 13:26:09 -0400

    copypasta

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -11,6 +11,10 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
   constructor(numBuckets = 8) {
     // Initialize your buckets here
     // Your code here
+    this.capacity= numBuckets;
+    this.data= new Array (numBuckets);
+    this.data.fill(null,0,numBuckets);
+    this.count=0;
   }
 
   hash(key) {
@@ -27,15 +31,46 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     // Get index after hashing
     return this.hash(key) % this.capacity;
   }
-
+  insertWithHashCollisions(key, value) {
+    // Your code here
+    const pair= new KeyValuePair(key,value)
+    const index= this.hashMod(key);
+    if (this.data[index]){
+      pair.next= this.data[index];
+      this.data[index]= pair;
+      this.count++;
+    } else{
+      this.data[index]= pair;
+      this.count++;
+    }
+  }
 
   insert(key, value) {
     // Your code here
+    let inserted = 0;
+    const pair= new KeyValuePair(key,value)
+    const index= this.hashMod(key);
+    if (this.data[index]){
+      let curr= this.data[index];
+
+      while (curr.next){
+        if (curr.key===key){
+          curr.value= value;
+          inserted++;
+        }
+        curr = curr.next;
+      }
+     }
+     if (inserted===0){
+      this.insertWithHashCollisions(key, value);
+     }
   }
 
 
   read(key) {
     // Your code here
+    const index= this.hashMod(key);
+    return (this.data[index])
   }
 
 
